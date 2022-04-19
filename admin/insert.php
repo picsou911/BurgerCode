aeb3411f45ced6c3e1210638740adf3e5eaa61bb
Jean-Paul Beaudonnet <jpjp03911@gmail.com>
2022-04-19 13:20:31 +0200

    Ajouter un item-Partie2

--- admin/insert.php
+++ admin/insert.php
@@ -1,5 +1,91 @@
 <?php
     require 'database.php';
+
+    $nameError = $descriptionError = $priceError = $categoryError = $imageError = $name = $description = $price = $category = $image = "";
+
+    if(!empty($_POST))
+    {
+        $name               = checkInput($_POST['name']);
+        $description        = checkInput($_POST['description']);
+        $price              = checkInput($_POST['price']);
+        $category           = checkInput($_POST['category']);
+        $image              = checkInput($_FILES['image']['name']);
+        $imagePath          = '../images/' . basename($image);
+        $imageExtension     = pathinfo($imagePath, PATHINFO_EXTENSION);
+        $isSuccess          = true;
+        $isUploadSuccess    = false;
+
+        if(empty($name))
+        {
+            $nameError = 'Ce champ ne peut pas être vide';
+            $isSuccess = false;
+        }
+        if(empty($description))
+        {
+            $descriptionError = 'Ce champ ne peut pas être vide';
+            $isSuccess = false;
+        }
+        if(empty($price))
+        {
+            $priceError = 'Ce champ ne peut pas être vide';
+            $isSuccess = false;
+        }
+        if(empty($category))
+        {
+            $categoryError = 'Ce champ ne peut pas être vide';
+            $isSuccess = false;
+        }
+        if(empty($image))
+        {
+            $nameError = 'Ce champ ne peut pas être vide';
+            $isSuccess = false;
+        }
+        else
+        {
+            $isUploadSuccess = true;
+            if($imageExtension != "jpg" && $imageExtension != "png" && $imageExtension != "jpeg" && $imageExtension != "gif")
+            {
+                $imageError = "Les fichiers autorises sont: .jpg, .jpeg, .png, .gif";
+                $isUploadSuccess = false;
+            }
+            if(file_exists($imagePath))
+            {
+                $imageError = "Le fichier existe deja";
+                $isUploadSuccess = false;
+            }
+            if($_FILES["image"]["size"] > 500000)
+            {
+                $imageError = "Le fichier ne doit pas depasser les 500KB";
+                $isUploadSuccess = false;
+            }
+            if($isUploadSuccess)
+            {
+                if(!move_uploaded_file($_FILES["image"]["tmp_name"], $imagePath))
+                {
+                    $imageError = "Il y a eu une erreur lors de l'upload";
+                    $isUploadSuccess = false;
+                }
+            }
+        }
+
+        if($isSuccess && $isUploadSuccess)
+        {
+            $db = Database::connect();
+            $statement = $db->prepare("INSERT INTO items (name,description,price,category,image) values(?, ?, ?, ?, ?)");
+            $statement->execute(array($name,$description,$price,$category,$image));
+            Database::disconnect();
+            header("Location: index.php");
+        }
+    
+    }
+
+    function checkInput($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -29,18 +115,17 @@
                         <span class="help-inline"><?php echo $nameError; ?></span>
                     </div>
                     <div class="form-group">
-                    </div>
-                    <label for="description">Description:</label>
+                        <label for="description">Description:</label>
                         <input type="text" class="form-control" id="description" name="description" placeholder="Description" value="<?php echo $description; ?>">
                         <span class="help-inline"><?php echo $descriptionError; ?></span>
                     </div>
                     <div class="form-group">
-                    <label for="price">Prix: (en €)</label>
+                        <label for="price">Prix: (en €)</label>
                         <input type="number" step="0.01" class="form-control" id="price" name="price" placeholder="Prix" value="<?php echo $price; ?>">
                         <span class="help-inline"><?php echo $priceError; ?></span>
                     </div>
                     <div class="form-group">
-                    <label for="category">Catégorie:</label>
+                        <label for="category">Catégorie:</label>
                         <select class="form-control" id="category" name="category">
                             <?php
                                 $db = Database::connect();
@@ -48,7 +133,7 @@
                                 {
                                     echo '<option value="' . $row['id'] . '">' . $row['name'] . '</option>';
                                 }
-                                $Database::disconnect();
+                                Database::disconnect();
                             ?>
                         </select>
                         <span class="help-inline"><?php echo $categoryError; ?></span> 
@@ -58,13 +143,12 @@
                         <input type="file" id="image" name="image">
                         <span class="help-inline"><?php echo $imageError;?></span>
                     </div>
-                </form>
                 <br>
                 <div class="form-actions">
                     <button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-pencil"></span> Ajouter</button>
                     <a class="btn btn-primary" href="index.php"><span class="glyphicon glyphicon-arrow-left"></span> Retour</a>
                 </div>
-                
+                </form>
                 
             </div>
         </div>
